846fe50c1409a9c44bb6c12df624a7727e2d81bd
hanbonghun <bng4535@gmail.com>
2023-08-07 01:36:56 +0900

    lettuce 적용

---
+++ src/main/java/com/example/stock/facade/LettuceLockStockFacade.java
@@ -0,0 +1,29 @@
+package com.example.stock.facade;
+
+import com.example.stock.repository.RedisLockRepository;
+import com.example.stock.service.StockService;
+import org.springframework.stereotype.Component;
+
+//구현이 간단하나 spinlock 방식으므로 redis에 부하를 줄 수 있는 단점 (Thread sleep 필요)
+@Component
+public class LettuceLockStockFacade {
+    private final RedisLockRepository redisLockRepository;
+    private final StockService stockService;
+
+    public LettuceLockStockFacade(RedisLockRepository redisLockRepository, StockService stockService) {
+        this.redisLockRepository = redisLockRepository;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(id)) {
+            Thread.sleep(100);
+        }
+
+        try{
+            stockService.decrease(id, quantity);
+        }finally {
+            redisLockRepository.unlock(id);
+        }
+    }
+}
--- src/main/java/com/example/stock/facade/NamedLockStockFacade.java
+++ src/main/java/com/example/stock/facade/NamedLockStockFacade.java
@@ -3,7 +3,6 @@ package com.example.stock.facade;
 import com.example.stock.repository.LockRepository;
 import com.example.stock.service.StockService;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
