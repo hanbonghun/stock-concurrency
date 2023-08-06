f83b9d62d93810a76b4982ca1f6607ec72f151f7
hanbonghun <bng4535@gmail.com>
2023-08-06 23:52:48 +0900

    synchronized를 활용한 동시성 처리

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -13,8 +13,14 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decrease(Long id, Long quantity) {
+
+    //@Transactional
+    //synchronized : 해당 메서드에는 하나의 쓰레드만 접근 가능
+    //그러나 @Transactional는 서비스를 wrapping한 클래스를 사용하는데 트랜잭션 종료 전에 다른 트랜잭션이 decrease를 호출 할 수 있음
+    //@Transactional을 주석 처리하면 테스트 통과
+    //즉 syncronized는 하나의 프로세스 안에서만 보장이 됨
+    //보통 여러대 서버를 사용하므로 잘 사용되지 않는다.
+    public synchronized void decrease(Long id, Long quantity) {
         // Stock 조회
         // 재고를 감소시킨 뒤
         // 갱신 값 저장
